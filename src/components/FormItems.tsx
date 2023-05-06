ace7f2123f73d4bb5d07910605da75843d329693
RusakovNikki <rusakov.nikki@yandex.ru>
2023-05-06 21:48:05 +0300

    add events by select

--- src/components/FormItems.tsx
+++ src/components/FormItems.tsx
@@ -1,8 +1,44 @@
 import React from "react"
-import Select from "react-select"
+import Select, { SingleValue } from "react-select"
 import { towers, levels, time, duration } from "../data"
+import { useAppDispatch, useAppSelector } from "../hooks"
+import { IOptionsSelect } from "../types"
+import {
+  setDurationBooking,
+  setLevelTower,
+  setTimeBooking,
+  setTypeOfTower,
+} from "../store/formSlice"
 
-const FormItems = () => {
+const FormItems: React.FC = () => {
+  const dispatch = useAppDispatch()
+
+  const data = useAppSelector((state) => state.form)
+
+  const onChangetower = (newValue: SingleValue<IOptionsSelect>) => {
+    if (newValue) {
+      dispatch(setTypeOfTower(newValue.value))
+    }
+  }
+  const onChangeLevels = (newValue: SingleValue<IOptionsSelect>) => {
+    if (newValue) {
+      dispatch(setLevelTower(newValue.value))
+    }
+  }
+
+  const onChangeTime = (newValue: SingleValue<IOptionsSelect>) => {
+    if (newValue) {
+      dispatch(setTimeBooking(newValue.value))
+    }
+  }
+
+  const onChangeDuration = (newValue: SingleValue<IOptionsSelect>) => {
+    if (newValue) {
+      dispatch(setDurationBooking(newValue.value))
+    }
+  }
+  console.log(data)
+
   return (
     <>
       <div className="main__form-item">
@@ -10,8 +46,8 @@ const FormItems = () => {
           Выбор башни
         </label>
         <Select
-          //   onChange={onChangetower}
           classNamePrefix="list"
+          onChange={onChangetower}
           placeholder=""
           options={towers}
         />
@@ -21,19 +57,34 @@ const FormItems = () => {
         <label className="main__label" htmlFor="react-select-3-input">
           Выбор этажа
         </label>
-        <Select classNamePrefix="list" placeholder="" options={levels} />
+        <Select
+          classNamePrefix="list"
+          onChange={onChangeLevels}
+          placeholder=""
+          options={levels}
+        />
       </div>
       <div className="main__form-item">
         <label className="main__label" htmlFor="react-select-4-input">
           Время начала
         </label>
-        <Select classNamePrefix="list" placeholder="" options={time} />
+        <Select
+          classNamePrefix="list"
+          onChange={onChangeTime}
+          placeholder=""
+          options={time}
+        />
       </div>
       <div className="main__form-item">
         <label className="main__label" htmlFor="react-select-5-input">
           Продолжительность
         </label>
-        <Select classNamePrefix="list" placeholder="" options={duration} />
+        <Select
+          classNamePrefix="list"
+          onChange={onChangeDuration}
+          placeholder=""
+          options={duration}
+        />
       </div>
     </>
   )
