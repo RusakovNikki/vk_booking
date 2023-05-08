94c5220f6a5080c8a8e968aafdba153d0abca5b4
RusakovNikki <rusakov.nikki@yandex.ru>
2023-05-08 18:44:18 +0300

    add notfoundpage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,21 @@
 import React from "react"
 
 const NotFoundPage = () => {
-  return <div>Ничего не найдено</div>
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        width: "100%",
+        height: "calc(100vh - 205px)",
+      }}
+    >
+      <h1>Произошла ошибка 😕</h1>
+      <h2>Такой страницы нет</h2>
+    </div>
+  )
 }
 
 export default NotFoundPage
